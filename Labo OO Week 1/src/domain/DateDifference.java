5b706da06de957ed797518b4d9072d4e6a6617cc
Wout <woutwoot13@gmail.com>
2014-09-19 12:56:23 +0200

    Started on day comparison stuff.

---
+++ Labo OO Week 1/src/domain/DateDifference.java
@@ -0,0 +1,11 @@
+package domain;
+
+import java.util.*;
+
+public class DateDifference {
+	
+	public static int daysBetween(Date d1, Date d2) {
+		return (int) ((d2.getTime() - d1.getTime()) / (1000 * 60 * 60 * 24));
+	}
+	
+}
--- Labo OO Week 1/src/domain/Item.java
+++ Labo OO Week 1/src/domain/Item.java
@@ -1,9 +1,12 @@
 package domain;
 
+import java.util.Date;
+
 public abstract class Item {
 	
 	protected String title;
-	protected int id;	
+	protected int id;
+	protected Date rentalDay;
 	
 	public Item(String title, int id) {
 		this.setTitle(title);
@@ -26,6 +29,22 @@ public abstract class Item {
 		this.title = title;
 	}
 
+	public Date getRentalDay(){
+		return this.rentalDay;
+	}
+	
+	public void setRentalDay(Date rentalDay){
+		this.rentalDay = rentalDay;
+	}
+	
+	public void setReturned(){
+		this.rentalDay = null;
+	}
+	
+	public int getDaysLent(){
+		return DateDifference.daysBetween(this.getRentalDay(), new Date());
+	}
+	
 	public abstract double getPrice(int days);
 
 }
