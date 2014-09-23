8815ab700506c041af3d4568f8d40396a43e0e2a
Wout <woutwoot13@gmail.com>
2014-09-23 14:40:49 +0200

    Items can now be leant and returned!

--- Labo OO Week 1/src/ui/WinkelUI.java
+++ Labo OO Week 1/src/ui/WinkelUI.java
@@ -1,5 +1,7 @@
 package ui;
 
+import java.util.Date;
+
 import javax.swing.JOptionPane;
 
 import util.IO;
@@ -39,6 +41,10 @@ public class WinkelUI {
 					showInventory(winkel);
 				} else if (choice == 5) {
 					showStatus(winkel);
+				} else if (choice == 6) {
+					lendItem(winkel);
+				} else if (choice == 7) {
+					returnItem(winkel);
 				} else if (choice == 0) {
 					go = false;
 				} else {
@@ -47,6 +53,34 @@ public class WinkelUI {
 			}
 		} catch (NumberFormatException e) {
 			show(winkel);
+		}
+	}
+
+	private void returnItem(Winkel winkel) {
+		int id = askId();
+		Item item = winkel.getItem(id);
+		if(item != null){
+			if(item.isLeant()){
+				JOptionPane.showMessageDialog(null, "Thank you for returning your Item!");
+				item.setReturned();
+			}
+		}else{
+			JOptionPane.showMessageDialog(null, "A Item with that id was not found.");
+		}
+	}
+
+	private void lendItem(Winkel winkel) {
+		int id = askId();
+		Item item = winkel.getItem(id);
+		if(item != null){
+			if(item.isLeant()){
+				JOptionPane.showMessageDialog(null, "This item is currently leant. You can not lend it.");
+			}else{
+				item.setRentalDay(new Date());
+				JOptionPane.showMessageDialog(null, "You have now leant the item.");
+			}
+		}else{
+			JOptionPane.showMessageDialog(null, "A Item with that id was not found.");
 		}
 	}
 
@@ -142,5 +176,5 @@ public class WinkelUI {
 		JOptionPane.showMessageDialog(null, items);
 	}
 
-	public static final String MENU = "1. Add item\n2. Show item\n3. Show rental price\n4. Show inventory\n5. Show item status\n\n0. Quit";
+	public static final String MENU = "1. Add item\n2. Show item\n3. Show rental price\n4. Show inventory\n5. Show item status\n6. Lend item\n7. Return item\n\n0. Quit";
 }
