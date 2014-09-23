35125cf7a3c9503aeaa1a4f4e49f124222cb7a07
Exevan <exevan110@gmail.com>
2014-09-23 14:22:28 +0200

    Added readInventory()

--- Labo OO Week 1/src/util/IO.java
+++ Labo OO Week 1/src/util/IO.java
@@ -1,20 +1,44 @@
 package util;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Scanner;
 
 import domain.Item;
 
 public class IO {
 
-	public static List<Item> readInventory(String path) {
+	public static List<Item> readInventory() {
+		List<String> data = read();
 		return null;
+	}
+	
+	private static List<String> read() {
+		List<String> data = new ArrayList<String>();
+		Scanner scanner = null;
+		try {
+			scanner = new Scanner(new File("winkel.txt"));
+			while(scanner.hasNext()) {
+				@SuppressWarnings("unused")
+				String line = scanner.next();
+				System.out.println(line);
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} finally {
+			scanner.close();
+		}
+		return data;
 	}
 	
 	@SuppressWarnings("unchecked")
@@ -34,9 +58,7 @@ public class IO {
 	private static void write(Collection<String> inventory) {
 		BufferedWriter writer = null;
 		try {
-			File file = new File("winkel.txt");
-			System.out.println(file.getCanonicalPath());
-			writer = new BufferedWriter(new FileWriter(file));			
+			writer = new BufferedWriter(new FileWriter(new File("winkel.txt")));			
 			for (String item : inventory) {
 				writer.write(item);
 			}
