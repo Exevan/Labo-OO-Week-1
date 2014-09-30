cb55d74fbc0d333b2a5e6d1a399875242815e242
Wout <woutwoot13@gmail.com>
2014-09-30 13:40:56 +0200

    Interfaces aangemaakt voor readers en schrijvers. Strategy.

--- Labo OO Week 1/src/db/WinkelDatabaseLezer.java
+++ Labo OO Week 1/src/db/WinkelDatabaseLezer.java
@@ -1,62 +1,7 @@
 package db;
 
-import java.io.FileNotFoundException;
-import java.util.Scanner;
+public interface WinkelDatabaseLezer {
 
-import domain.DomainException;
-import domain.Film;
-import domain.Muziek;
-import domain.Product;
-import domain.Spel;
-import domain.Winkel;
-
-public class WinkelDatabaseLezer extends WinkelDatabaseHandler {
+	public void lees();
 	
-	
-	public WinkelDatabaseLezer(String filename, Winkel winkel)
-		throws DbException
-	{
-		super(filename,winkel);
-	}
-
-	public void lees()
-		throws DbException{		
-		Scanner filescanner = null;
-		try {
-			filescanner = new Scanner(getBestand());
-		} catch (FileNotFoundException e1) {
-			throw new DbException("file not available");
-		}
-		while(filescanner.hasNextLine())
-		{
-			Scanner linescanner = new Scanner(filescanner.nextLine());
-			linescanner.useDelimiter("\t");
-			String type = linescanner.next();
-			String id = linescanner.next();
-			String title = linescanner.next();
-			Product product = null;
-			try {
-				switch (type) {
-				case "F":
-					product = new Film(id, title);
-					break;
-				case "M":
-					product = new Muziek(id, title);
-					break;
-				case "S":
-					product = new Spel(id, title);
-					break;
-				default:
-					break;
-				}
-				getWinkel().voegProductToe(product);
-			} catch (DomainException e) {
-				throw new DbException("cannot add product available in file");
-			} finally {
-				linescanner.close();
-			}						
-		}
-		filescanner.close();
-	}
-
 }
--- Labo OO Week 1/src/db/WinkelDatabaseSchrijver.java
+++ Labo OO Week 1/src/db/WinkelDatabaseSchrijver.java
@@ -1,40 +1,7 @@
 package db;
 
-import java.io.FileNotFoundException;
-import java.io.PrintWriter;
-import java.util.ArrayList;
-
-import domain.Product;
-import domain.Winkel;
-
-public class WinkelDatabaseSchrijver extends WinkelDatabaseHandler{
-
-	public WinkelDatabaseSchrijver(String filename, Winkel winkel)
-		throws DbException
-	{
-		super(filename,winkel);
-	}
-
-		
-	public void schrijf()
-		throws DbException{	
-		PrintWriter schrijver;
-		try{
-			schrijver = new PrintWriter(getBestand());
-		}catch(FileNotFoundException e){
-			throw new DbException("file not available");
-		}
-		ArrayList<Product> producten = getWinkel().getProducten();
-		for(Product product: producten)
-		{
-			schrijver.print(product.getType());			
-			schrijver.print("\t");
-			//id
-			schrijver.print(product.getId());
-			schrijver.print("\t");
-			//title
-			schrijver.println(product.getName());
-		}
-		schrijver.close();		
-	}
+public interface WinkelDatabaseSchrijver {
+	
+	public void schrijf();
+	
 }
